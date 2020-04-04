1743096be97fb2f4d7fda2a89203b4378ea4cddd
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2020-04-05 04:18:17 +0530

    Autowiring demo without setter/constructor injection

--- conference - java - configured/src/main/java/AppConfig.java
+++ conference - java - configured/src/main/java/AppConfig.java
@@ -14,20 +14,31 @@ public class AppConfig {
     //1. Singleton : by default
         //Single instance per spring container
     //2. Prototype
+        //Unique bean per request
     //3. Request
+        //Unique object per HTTP request
     //4. Session
+        //Object scope stays till the time a session is live.
     //5. Global
+        //Uniques object per application, as from the time of deployement to reboot or undeployement
 
     @Bean(name = "speakerService")
-//    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
-    @Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
+//    @Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
     public SpeakerService getSpeakerService(){
 
         //Constructor Injection
-        SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
+//        SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
+
+        /**
+         * For autowiring demo
+         * we have commented both setter and constructor injection
+         * and added @Autowired annotation to setter method
+         */
+        SpeakerServiceImpl service = new SpeakerServiceImpl();
 
         //Setter Injection
-        //service.setRepository(getSpeakerRepository());
+//        service.setRepository(getSpeakerRepository());
         return service;
     }
 
--- conference - java - configured/src/main/java/com/pulkit/service/SpeakerServiceImpl.java
+++ conference - java - configured/src/main/java/com/pulkit/service/SpeakerServiceImpl.java
@@ -3,6 +3,7 @@ package com.pulkit.service;
 import com.pulkit.model.Speaker;
 import com.pulkit.repository.HibernateSpeakerRepositoryImpl;
 import com.pulkit.repository.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
@@ -10,15 +11,23 @@ public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository repository;
 
+    public SpeakerServiceImpl(){
+        System.out.println("SpeakerServiceImpl no args constructor");
+    }
     public SpeakerServiceImpl(SpeakerRepository speakerRepository){
         this.repository = speakerRepository;
+        System.out.println("SpeakerServiceImpl repository constructor");
+
     }
     @Override
     public List<Speaker> findAll(){
         return repository.findAll();
     }
 
+    @Autowired
     public void setRepository(SpeakerRepository repository) {
+
         this.repository = repository;
+        System.out.println("SpeakerServiceImpl setter");
     }
 }
